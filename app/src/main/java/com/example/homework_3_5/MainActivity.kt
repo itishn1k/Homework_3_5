63647a35ba0137cda4bd574bba368ecf19632327
itishn1k <kayrat1224mamataev@gmail.com>
2022-11-21 22:28:03 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework_3_5/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.homework_3_5
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.homework_3_5.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+private var counter = 0
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            counter++
+            binding.textView.text = counter.toString()
+            if (counter == 10) {
+                binding.button.text = applicationContext.getString(R.string.minus_1)
+                binding.button.setOnClickListener {
+                    counter--
+                    binding.textView.text = counter.toString()
+                    if (counter == 0) {
+                        binding.button.setOnClickListener {
+                            val intent = Intent(this, NewActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
