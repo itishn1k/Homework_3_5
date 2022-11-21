2980bde0d5a683b52b40c07d1230905aae05b0a7
itishn1k <kayrat1224mamataev@gmail.com>
2022-11-21 22:31:50 +0600

    ЬРЬОРОЛ

--- app/src/main/java/com/example/homework_3_5/MainActivity.kt
+++ app/src/main/java/com/example/homework_3_5/MainActivity.kt
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.homework_3_5.databinding.ActivityMainBinding
 
-private lateinit var binding: ActivityMainBinding
-private var counter = 0
-
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
